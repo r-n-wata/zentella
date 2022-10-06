f60fd75cb62437197e21a5323d09349ee514e873
Ruth Wata <ruthnwata@gmail.com>
2022-10-06 13:43:40 -0500

    removed gallery image

--- src/data/carouselImgs.js
+++ src/data/carouselImgs.js
@@ -1,4 +1,4 @@
-const image1 = require('../imgs/image1.JPG')
+// const image1 = require('../imgs/image1.JPG')
 const image2 = require('../imgs/image2.JPG')
 const image3 = require('../imgs/image3.JPG')
 const image4 = require('../imgs/image4.JPG')
@@ -9,10 +9,10 @@ export const carouselImages = () => {
     
     return ([
 
-        {
-            'image':image1,
-            'key':1
-        },
+        // {
+        //     'image':image1,
+        //     'key':1
+        // },
         {
             'image':image2,
             'key':2
