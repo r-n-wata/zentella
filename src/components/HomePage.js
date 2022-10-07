45e6172c9449bdaeba54926129367b88932acbd9
Ruth Wata <ruthnwata@gmail.com>
2022-10-07 11:36:54 -0500

    changed width of social links

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,13 +12,13 @@ export default function HomePage(){
 
                 <div className='flex flex-col gap-8 justify-end pb-6 ml-8  h-full w-full  desktop:pl-4'>
 
-                    <a href='https://www.facebook.com/jzentella'><img src='/images/icons/icons8-facebook-50.png' alt='facebook--link' className='w-8  '/></a>
+                    <a href='https://www.facebook.com/jzentella'><img src='/images/icons/icons8-facebook-50.png' alt='facebook--link' className='w-6 desktop:w-8  '/></a>
 
-                    <a href='http://instagram.com/zentella_ja'><img src='/images/icons/icons8-instagram-24.png' alt='instagram--link' className='w-8 '/></a>
+                    <a href='http://instagram.com/zentella_ja'><img src='/images/icons/icons8-instagram-24.png' alt='instagram--link' className='w-6 desktop:w-8 '/></a>
 
                     {/* <a href=''><img src='/images/icons/icons8-youtube-24.png' alt='youtube--link' className='main--social-icon'/></a> */}
 
-                    <a href='https://soundcloud.com/javier-zentella'><img src='/images/icons/icons8-soundcloud-50.png' alt='soundcloud--link' className='w-8 '/></a>
+                    <a href='https://soundcloud.com/javier-zentella'><img src='/images/icons/icons8-soundcloud-50.png' alt='soundcloud--link' className='w-6 desktop:w-8 '/></a>
                 </div>
 
             </div>
